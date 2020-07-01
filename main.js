8d31eca61e7cfecb9080dd694ce15065f225bdd8
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-06-30 23:37:17 -0700

    Homework

--- main.js
+++ main.js
@@ -2,6 +2,78 @@
  * YOUR CODE HERE *
  ******************/
 
+const indexedChars = function (str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    result = result + i + str[i] 
+  }
+  return result;
+}
+
+const exclaim = function (str) {
+  let result = '';
+  
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    if (currentChar === '?') {
+      result = result  + '!' 
+    } else if (currentChar === ".") {
+      result = result + '!'
+    } else {
+    result = result + currentChar }
+    
+  }
+    return result;
+  }
+
+  const repeatIt = function (str, count) {
+    let repeatedStr = '';
+      for (let i = 0; i < count; i++) {
+        repeatedStr += str;
+      }
+   
+    return repeatedStr;
+  }
+
+  const truncate = function (str) {
+    let result = ''; 
+     for (let i = 0; i < str.length; i++) {
+       if (str.length < 18) {
+         result = str
+       } else {
+         result = str.substring(0, 15) + '...'
+       }
+     }
+     return result;
+    }
+
+    const ciEmailify = function (str) {
+      let result = "";
+      let email = "@codeimmersives.com";
+
+        for (let i = 0; i === str.length; i++) {
+          result = result + str.toLowerCase() +  '.' + email;
+        }
+    }
+
+    const reverse = function (str) {
+      let result = "";
+        for (let i = str.length - 1; i >= 0; i--) {
+          result += str[i];
+        }
+        return result
+    }
+
+    const onlyVowels = function (str) {
+      let result = "";
+      let vowels ='AEIOUaeiou'
+      for (let i = vowels; i <= str.length; i++) {
+       result += str[i]
+    }
+      return result
+    }
+
 
 
 
